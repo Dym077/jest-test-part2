48982d727b58996e5a57c64cb7bdaca23c972f7d
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-02-23 13:21:06 +0000

    Add addTurn function

--- scripts/game.js
+++ scripts/game.js
@@ -12,10 +12,17 @@ function newGame() {
     game.currentGame = [];
     game.score = 0;
     showScore();
+    addTurn();
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
 }
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore };
+module.exports = { game, newGame, showScore, addTurn,  };
